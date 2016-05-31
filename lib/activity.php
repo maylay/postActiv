db3fc44a6796b9b368a86f73373654b537351c81
Maiyannah Bishop <maiyannah@highlandarrow.com>
2016-05-31 18:37:07 -0400

    Consolidate ActivitySink and ActivitySource into /lib/activity.php as well

--- lib/activity.php
+++ lib/activity.php
@@ -17,6 +17,9 @@
  * ActivityImporter abstracts a means that is importing activity verbs
  * into the system as part of a user's timeline.
  * ActivityMover abstracts the means to transport activity verbs.
+ * ActivitySink abstracts a class to receive activity verbs.
+ * ActivitySource abstracts a class to represent the source of a received
+ *    activity verb.
  *
  * ActivityObject is a noun in the activity universe basically, from
  * the original file:
@@ -29,7 +32,7 @@
  *     This is just a thing in the activity universe. Can be the subject,
  *     object, or indirect object (target!) of an activity verb. Rotten
  *     name, and I'm propagating it. *sigh*
- * It's large enough that I've left it seperate.
+ * It's large enough that I've left it seperate in activityobject.php
  *
  *
  * PHP version 5
@@ -439,7 +442,7 @@ class Activity
                 $activity['object']['attachments'][] = $attachment->asArray();
             }
         }
-        
+
         // Context stuff.
 
         if (!empty($this->context)) {
@@ -677,7 +680,7 @@ class Activity
 
             foreach ($this->context->attention as $attnURI=>$type) {
                 $xs->element('link', array('rel' => ActivityContext::MENTIONED,
-                                           ActivityContext::OBJECTTYPE => $type,  // FIXME: undocumented 
+                                           ActivityContext::OBJECTTYPE => $type,  // FIXME: undocumented
                                            'href' => $attnURI));
             }
 
@@ -1399,5 +1402,144 @@ class ActivityMover extends QueueHandler
     {
         common_log($level, "ActivityMover: " . $message);
     }
+}
+
+class ActivitySink
+{
+    protected $svcDocUrl   = null;
+    protected $username    = null;
+    protected $password    = null;
+    protected $collections = array();
+
+    function __construct($svcDocUrl, $username, $password)
+    {
+        $this->svcDocUrl = $svcDocUrl;
+        $this->username  = $username;
+        $this->password  = $password;
+
+        $this->_parseSvcDoc();
+    }
+
+    private function _parseSvcDoc()
+    {
+        $client   = new HTTPClient();
+        $response = $client->get($this->svcDocUrl);
+
+        if ($response->getStatus() != 200) {
+            throw new Exception("Can't get {$this->svcDocUrl}; response status " . $response->getStatus());
+        }
+
+        $xml = $response->getBody();
+
+        $dom = new DOMDocument();
+
+        // We don't want to bother with white spaces
+        $dom->preserveWhiteSpace = false;
+
+        // Don't spew XML warnings to output
+        $old = error_reporting();
+        error_reporting($old & ~E_WARNING);
+        $ok = $dom->loadXML($xml);
+        error_reporting($old);
+
+        $path = new DOMXPath($dom);
+
+        $path->registerNamespace('atom', 'http://www.w3.org/2005/Atom');
+        $path->registerNamespace('app', 'http://www.w3.org/2007/app');
+        $path->registerNamespace('activity', 'http://activitystrea.ms/spec/1.0/');
+
+        $collections = $path->query('//app:collection');
+
+        for ($i = 0; $i < $collections->length; $i++) {
+            $collection = $collections->item($i);
+            $url = $collection->getAttribute('href');
+            $takesEntries = false;
+            $accepts = $path->query('app:accept', $collection);
+            for ($j = 0; $j < $accepts->length; $j++) {
+                $accept = $accepts->item($j);
+                $acceptValue = $accept->nodeValue;
+                if (preg_match('#application/atom\+xml(;\s*type=entry)?#', $acceptValue)) {
+                    $takesEntries = true;
+                    break;
+                }
+            }
+
+            if (!$takesEntries) {
+                continue;
+            }
+            $verbs = $path->query('activity:verb', $collection);
+            if ($verbs->length == 0) {
+                $this->_addCollection(ActivityVerb::POST, $url);
+            } else {
+                for ($k = 0; $k < $verbs->length; $k++) {
+                    $verb = $verbs->item($k);
+                    $this->_addCollection($verb->nodeValue, $url);
+                }
+            }
+        }
+    }
+
+    private function _addCollection($verb, $url)
+    {
+        if (array_key_exists($verb, $this->collections)) {
+            $this->collections[$verb][] = $url;
+        } else {
+            $this->collections[$verb] = array($url);
+        }
+        return;
+    }
+
+    function postActivity($activity)
+    {
+        if (!array_key_exists($activity->verb, $this->collections)) {
+            throw new Exception("No collection for verb {$activity->verb}");
+        } else {
+            if (count($this->collections[$activity->verb]) > 1) {
+                common_log(LOG_NOTICE, "More than one collection for verb {$activity->verb}");
+            }
+            $this->postToCollection($this->collections[$activity->verb][0], $activity);
+        }
+    }
+
+    function postToCollection($url, $activity)
+    {
+        $client = new HTTPClient($url);
+
+        $client->setMethod('POST');
+        $client->setAuth($this->username, $this->password);
+        $client->setHeader('Content-Type', 'application/atom+xml;type=entry');
+        $client->setBody($activity->asString(true, true, true));
+
+        $response = $client->send();
+
+        $status = $response->getStatus();
+        $reason = $response->getReasonPhrase();
+
+        if ($status >= 200 && $status < 300) {
+            return true;
+        } else if ($status >= 400 && $status < 500) {
+            // TRANS: Client exception thrown when post to collection fails with a 400 status.
+            // TRANS: %1$s is a URL, %2$s is the status, %s$s is the fail reason.
+            throw new ClientException(sprintf(_m('URLSTATUSREASON','%1$s %2$s %3$s'), $url, $status, $reason));
+        } else if ($status >= 500 && $status < 600) {
+            // TRANS: Server exception thrown when post to collection fails with a 500 status.
+            // TRANS: %1$s is a URL, %2$s is the status, %s$s is the fail reason.
+            throw new ServerException(sprintf(_m('URLSTATUSREASON','%1$s %2$s %3$s'), $url, $status, $reason));
+        } else {
+            // That's unexpected.
+            // TRANS: Exception thrown when post to collection fails with a status that is not handled.
+            // TRANS: %1$s is a URL, %2$s is the status, %s$s is the fail reason.
+            throw new Exception(sprintf(_m('URLSTATUSREASON','%1$s %2$s %3$s'), $url, $status, $reason));
+        }
+    }
+}
+
+class ActivitySource
+{
+    public $id;
+    public $title;
+    public $icon;
+    public $updated;
+    public $links;
 }
 ?>
