11a3bed18ae78ed70bb7c69c3e37f4f198dfce92
Maiyannah Bishop <maiyannah@highlandarrow.com>
2016-06-01 04:28:14 -0400

    prepare() strict standards compliance for plugins/Poll

--- plugins/Poll/actions/respondpoll.php
+++ plugins/Poll/actions/respondpoll.php
@@ -70,9 +70,9 @@ class RespondPollAction extends Action
      *
      * @return boolean true
      */
-    function prepare($argarray)
+    function prepare(array $args = array())
     {
-        parent::prepare($argarray);
+        parent::prepare($args);
         if ($this->boolean('ajax')) {
             GNUsocial::setApi(true);
         }
