913acea1325f3cd5bcf17374a15c4f00acb31961
Maiyannah Bishop <maiyannah@highlandarrow.com>
2016-06-04 18:38:41 -0400

    Authorship audit in /actions/ (pass 2)

--- actions/apigroupcreate.php
+++ actions/apigroupcreate.php
@@ -25,10 +25,13 @@
  *
  * @category  API
  * @package   postActiv
- * @author    Craig Andrews <candrews@integralblue.com>
- * @author    Evan Prodromou <evan@status.net>
- * @author    Jeffery To <jeffery.to@gmail.com>
  * @author    Zach Copley <zach@status.net>
+ * @author    Evan Prodromou <evan@prodromou.name>
+ * @author    Siebrand Mazeland <s.mazeland@xs4all.nl>
+ * @author    Eric Helgeson <erichelgeson@gmail.com>
+ * @author    Craig Andrews <candrews@integralblue.com>
+ * @author    Brion Vibber <brion@pobox.com>
+ * @author    Mikael Nordfeldth <mmn@hethane.se>
  * @author    Maiyannah Bishop <maiyannah@member.fsf.org>
  * @copyright 2008-2011 StatusNet, Inc.
  * @copyright 2013-2016 Free Software Foundation, Inc.
