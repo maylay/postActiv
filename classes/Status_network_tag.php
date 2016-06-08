ad8e846d0897ffd27c58b294e7ce2694840043ca
Maiyannah Bishop <maiyannah.bishop@postactiv.com>
2016-06-08 16:02:21 -0400

    Authorship audit in /classes/ (pass 5)

--- classes/Status_network_tag.php
+++ classes/Status_network_tag.php
@@ -6,7 +6,7 @@
  *   GNU Social (C) 2013-2016, Free Software Foundation, Inc
  *   StatusNet (C) 2008-2011, StatusNet, Inc
  *
- * PHP version 5 
+ * PHP version 5
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
@@ -21,7 +21,19 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- * @author Maiyannah Bishop <maiyannah.bishop@postactiv.com>
+ * @category  Networking
+ * @package   postActiv
+ * @author    James Walker <walkah@walkah.net>
+ * @author    Brion Vibber <brion@pobox.com>
+ * @author    Siebrand Mazeland <s.mazeland@xs4all.nl>
+ * @author    Evan Prodromou <evan@prodromou.name>
+ * @author    Mikeal Nordfeldth <mmn@hethane.se>
+ * @author    Maiyannah Bishop <maiyannah.bishop@postactiv.com>
+ * @copyright 2010-2011 StatusNet, Inc.
+ * @copyright 2013-2016 Free Software Foundation, Inc.
+ * @copyright 2016 Maiyannah Bishop
+ * @license   http://www.fsf.org/licensing/licenses/agpl-3.0.html
+ * @link      https://git.gnu.io/maiyannah/postActiv
  */
 
 if (!defined('STATUSNET')) { exit(1); }
