095e069d85f7cd7a6c65b0ca05bb501d64733c4c
Maiyannah Bishop <maiyannah.bishop@postactiv.com>
2016-09-25 19:32:38 -0400

    Some very basic work for federation plugins

---
+++ plugins/FederateMyspace/cron/RemoteFetch.php
@@ -0,0 +1,4 @@
+<?php
+// Myspace doesn't have a remote notify method automatically, so we have to occasionally fetch new notices.
+// This file will do so when placed on a crontab, and well, its completed, that's important too.
+?>
