c6ab24e47fde26fbe1e737af55e292d84518007d
Maiyannah Bishop <maiyannah.bishop@postactiv.com>
2016-11-10 23:55:21 -0500

    Moving the federate* plugins to seperate repositories.

--- plugins/FederateMyspace/classes/MyspaceLocalUser.php
+++
@@ -1,3 +0,0 @@
-<?php
-// A myspace user with a local account linked, so they're not foreign to the instance
-?>
--- plugins/FederateMyspace/cron/RemoteFetch.php
+++
@@ -1,4 +0,0 @@
-<?php
-// Myspace doesn't have a remote notify method automatically, so we have to occasionally fetch new notices.
-// This file will do so when placed on a crontab, and well, its completed, that's important too.
-?>
