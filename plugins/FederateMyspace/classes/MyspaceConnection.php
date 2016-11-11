104cf2dfbe6fde9b3b37afe7ec6989d99e521bd7
Maiyannah Bishop <maiyannah.bishop@postactiv.com>
2016-11-10 23:55:21 -0500

    Moving the federate* plugins to seperate repositories.

--- plugins/FederateMyspace/classes/MyspaceConnection.php
+++
@@ -1,3 +0,0 @@
-<?php
-// Record of a myspace oauth authorization token
-?>
--- plugins/FederateMyspace/classes/MyspaceRemoteUser.php
+++
@@ -1,4 +0,0 @@
-<?php
-// A Myspace user which does not have a local user linked, and is thus foreign to the instance
-// !! make sure we have a way to merge/change it to a local user if someone links the account !!
-?>
