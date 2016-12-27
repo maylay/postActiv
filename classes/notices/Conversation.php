7efb446c406e62cb56690392ef5b35c10e7d6537
Maiyannah Bishop <maiyannah.bishop@postactiv.com>
2016-12-26 23:09:23 -0500

    Documentation for Conversation class

--- classes/notices/Conversation.php
+++ classes/notices/Conversation.php
@@ -56,134 +56,194 @@
 if (!defined('POSTACTIV')) { exit(1); }
 
 
+// ----------------------------------------------------------------------------
+// Class: Conversation
+// Base superclass for Conversations
+//
+// Variables:
+// o $__table  - table name ('conversation')
+// o $id       - int(4)  primary_key not_null auto_increment
+// o $uri      - varchar(191)  unique_key   not 255 because utf8mb4 takes more space
+// o $created  - datetime   not_null
+// o $modified - timestamp   not_null default_CURRENT_TIMESTAMP
 class Conversation extends Managed_DataObject
 {
-    public $__table = 'conversation';        // table name
-    public $id;                              // int(4)  primary_key not_null auto_increment
-    public $uri;                             // varchar(191)  unique_key   not 255 because utf8mb4 takes more space
-    public $created;                         // datetime   not_null
-    public $modified;                        // timestamp   not_null default_CURRENT_TIMESTAMP
+   public $__table = 'conversation';        // table name
+   public $id;                              // int(4)  primary_key not_null auto_increment
+   public $uri;                             // varchar(191)  unique_key   not 255 because utf8mb4 takes more space
+   public $created;                         // datetime   not_null
+   public $modified;                        // timestamp   not_null default_CURRENT_TIMESTAMP
 
-    public static function schemaDef()
-    {
-        return array(
-            'fields' => array(
-                'id' => array('type' => 'serial', 'not null' => true, 'description' => 'Unique identifier, (again) unrelated to notice id since 2016-01-06'),
-                'uri' => array('type' => 'varchar', 'not null'=>true, 'length' => 191, 'description' => 'URI of the conversation'),
-                'created' => array('type' => 'datetime', 'not null' => true, 'description' => 'date this record was created'),
-                'modified' => array('type' => 'timestamp', 'not null' => true, 'description' => 'date this record was modified'),
-            ),
-            'primary key' => array('id'),
-            'unique keys' => array(
-                'conversation_uri_key' => array('uri'),
-            ),
-        );
-    }
 
-    static public function beforeSchemaUpdate()
-    {
-        $table = strtolower(get_called_class());
-        $schema = Schema::get();
-        $schemadef = $schema->getTableDef($table);
+   // -------------------------------------------------------------------------
+   // Function: schemaDef
+   // Returns an array with the table schema definition
+   public static function schemaDef() {
+      return array(
+         'fields' => array(
+            'id' => array('type' => 'serial', 'not null' => true, 'description' => 'Unique identifier, (again) unrelated to notice id since 2016-01-06'),
+            'uri' => array('type' => 'varchar', 'not null'=>true, 'length' => 191, 'description' => 'URI of the conversation'),
+            'created' => array('type' => 'datetime', 'not null' => true, 'description' => 'date this record was created'),
+            'modified' => array('type' => 'timestamp', 'not null' => true, 'description' => 'date this record was modified')),
+         'primary key' => array('id'),
+         'unique keys' => array(
+            'conversation_uri_key' => array('uri')));
+   }
 
-        // 2016-01-06 We have to make sure there is no conversation with id==0 since it will screw up auto increment resequencing
-        if ($schemadef['fields']['id']['auto_increment']) {
-            // since we already have auto incrementing ('serial') we can continue
-            return;
-        }
 
-        // The conversation will be recreated in upgrade.php, which will
-        // generate a new URI, but that's collateral damage for you.
-        $conv = new Conversation();
-        $conv->id = 0;
-        if ($conv->find()) {
-            while ($conv->fetch()) {
-                // Since we have filtered on 0 this only deletes such entries
-                // which I have been afraid wouldn't work, but apparently does!
-                // (I thought it would act as null or something and find _all_ conversation entries)
-                $conv->delete();
-            }
-        }
-    }
+   // -------------------------------------------------------------------------
+   // Function: beforeSchemaUpdate
+   // Cleanup to do prior to database schema checks.
+   static public function beforeSchemaUpdate()
+   {
+      $table = strtolower(get_called_class());
+      $schema = Schema::get();
+      $schemadef = $schema->getTableDef($table);
 
-    /**
-     * Factory method for creating a new conversation.
-     *
-     * Use this for locally initiated conversations. Remote notices should
-     * preferrably supply their own conversation URIs in the OStatus feed.
-     *
-     * @return Conversation the new conversation DO
-     */
-    static function create($uri=null, $created=null)
-    {
-        // Be aware that the Notice does not have an id yet since it's not inserted!
-        $conv = new Conversation();
-        $conv->created = $created ?: common_sql_now();
-        $conv->uri = $uri ?: sprintf('%s%s=%s:%s=%s',
-                             TagURI::mint(),
-                             'objectType', 'thread',
-                             'nonce', common_random_hexstr(8));
-        // This insert throws exceptions on failure
-        $conv->insert();
+      // 2016-01-06 We have to make sure there is no conversation with id==0 since it will screw up auto increment resequencing
+      if ($schemadef['fields']['id']['auto_increment']) {
+         // since we already have auto incrementing ('serial') we can continue
+         return;
+      }
 
-        return $conv;
-    }
+      // The conversation will be recreated in upgrade.php, which will
+      // generate a new URI, but that's collateral damage for you.
+      $conv = new Conversation();
+      $conv->id = 0;
+      if ($conv->find()) {
+         while ($conv->fetch()) {
+            // Since we have filtered on 0 this only deletes such entries
+            // which I have been afraid wouldn't work, but apparently does!
+            // (I thought it would act as null or something and find _all_ conversation entries)
+            $conv->delete();
+         }
+      }
+   }
 
-    static function noticeCount($id)
-    {
-        $keypart = sprintf('conversation:notice_count:%d', $id);
 
-        $cnt = self::cacheGet($keypart);
+   // -------------------------------------------------------------------------
+   // Function: create
+   // Factory method for creating a new conversation.
+   //
+   // Use this for locally initiated conversations. Remote notices should
+   // preferrably supply their own conversation URIs in the OStatus feed.
+   //
+   // Returns:
+   // o Conversation $conv - the new conversation DO
+   static function create($uri=null, $created=null) {
+      // Be aware that the Notice does not have an id yet since it's not inserted!
+      $conv = new Conversation();
+      $conv->created = $created ?: common_sql_now();
+      $conv->uri = $uri ?: sprintf('%s%s=%s:%s=%s',
+                           TagURI::mint(),
+                           'objectType', 'thread',
+                           'nonce', common_random_hexstr(8));
+      // This insert throws exceptions on failure
+      $conv->insert();
+      return $conv;
+   }
 
-        if ($cnt !== false) {
-            return $cnt;
-        }
 
-        $notice               = new Notice();
-        $notice->conversation = $id;
-        $notice->whereAddIn('verb', array(ActivityVerb::POST, ActivityUtils::resolveUri(ActivityVerb::POST, true)), $notice->columnType('verb'));
-        $cnt                  = $notice->count();
+   // -------------------------------------------------------------------------
+   // Function: noticeCount
+   // How many notices are in this conversation?
+   //
+   // Returns:
+   // o integer $cnt
+   static function noticeCount($id)
+   {
+      $keypart = sprintf('conversation:notice_count:%d', $id);
+      $cnt = self::cacheGet($keypart);
+      if ($cnt !== false) {
+         return $cnt;
+      }
+      $notice               = new Notice();
+      $notice->conversation = $id;
+      $notice->whereAddIn('verb', array(ActivityVerb::POST, ActivityUtils::resolveUri(ActivityVerb::POST, true)), $notice->columnType('verb'));
+      $cnt                  = $notice->count();
+      self::cacheSet($keypart, $cnt);
+      return $cnt;
+   }
 
-        self::cacheSet($keypart, $cnt);
 
-        return $cnt;
-    }
+   // -------------------------------------------------------------------------
+   // Function: getUrlFromNotice
+   // Get the URL of the specified notice in the conversation.
+   //
+   // Parameters:
+   // o Notice $notice
+   // o boolean $anchor (default TRUE)
+   //
+   // Returns:
+   // o Url of Notice
+   static public function getUrlFromNotice(Notice $notice, $anchor=true) {
+      $conv = Conversation::getByID($notice->conversation);
+      return $conv->getUrl($anchor ? $notice->getID() : null);
+   }
 
-    static public function getUrlFromNotice(Notice $notice, $anchor=true)
-    {
-        $conv = Conversation::getByID($notice->conversation);
-        return $conv->getUrl($anchor ? $notice->getID() : null);
-    }
 
-    public function getUri()
-    {
-        return $this->uri;
-    }
+   // -------------------------------------------------------------------------
+   // Function: getUri
+   // Get the URI of the conversation
+   //
+   // Returns:
+   // o string $uri
+   public function getUri() {
+      return $this->uri;
+   }
 
-    public function getUrl($noticeId=null)
-    {
-        // FIXME: the URL router should take notice-id as an argument...
-        return common_local_url('conversation', array('id' => $this->getID())) .
-                ($noticeId===null ? '' : "#notice-{$noticeId}");
-    }
 
-    // FIXME: ...will 500 ever be too low? Taken from ConversationAction::MAX_NOTICES
-    public function getNotices(Profile $scoped=null, $offset=0, $limit=500)
-    {
-        $stream = new ConversationNoticeStream($this->getID(), $scoped);
-        $notices = $stream->getNotices($offset, $limit);
-        return $notices;
-    }
+   // -------------------------------------------------------------------------
+   // Function: getUrl
+   // Get the URL of the conversation
+   //
+   // Parameters:
+   // o integer noticeId (default NULL)
+   //
+   // Returns:
+   // o string $url - the URL location of this conversation
+   public function getUrl($noticeId=null) {
+      // FIXME: the URL router should take notice-id as an argument...
+      return common_local_url('conversation', array('id' => $this->getID())) .
+         ($noticeId===null ? '' : "#notice-{$noticeId}");
+   }
 
-    public function insert()
-    {
-        $result = parent::insert();
-        if ($result === false) {
-            common_log_db_error($this, 'INSERT', __FILE__);
-            throw new ServerException(_('Failed to insert Conversation into database'));
-        }
-        return $result;
-    }
+   
+   // -------------------------------------------------------------------------
+   // Function: getNotices
+   // Retrieve the notices of the conversation.
+   //
+   // FIXME: 
+   // ...will 500 ever be too low? Taken from ConversationAction::MAX_NOTICES
+   //
+   // Parameters:
+   // o Profile $scoped (default NULL)
+   // o integer $offset (default 0)
+   // o integer $limit (default 500)
+   //
+   // Returns:
+   // o array of Notice objects
+   public function getNotices(Profile $scoped=null, $offset=0, $limit=500) {
+      $stream = new ConversationNoticeStream($this->getID(), $scoped);
+      $notices = $stream->getNotices($offset, $limit);
+      return $notices;
+   }
+
+
+   // -------------------------------------------------------------------------
+   // Function:
+   // Insert a new conversation record into the database
+   //
+   // Error States:
+   // o raises a ServerException if inserting the record fails
+   public function insert() {
+      $result = parent::insert();
+      if ($result === false) {
+         common_log_db_error($this, 'INSERT', __FILE__);
+         throw new ServerException(_('Failed to insert Conversation into database'));
+      }
+      return $result;
+   }
 }
 
 // END OF FILE
