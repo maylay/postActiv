571cd6a9752c920c20f692ef4091094bb69181a5
Maiyannah Bishop <maiyannah.bishop@postactiv.com>
2016-12-28 04:26:31 -0500

    Move ActivityVerb classes into core software per #61

--- classes/plugins/ActivityVerbHandlerPlugin.php
+++ classes/plugins/ActivityVerbHandlerPlugin.php
@@ -1,11 +1,19 @@
 <?php
-/*
- * postActiv - a fork of the GNU Social microblogging software
- * Copyright (C) 2016, Maiyannah Bishop
- * Derived from code copyright various sources:
- *   GNU Social (C) 2013-2016, Free Software Foundation, Inc
- *   StatusNet (C) 2008-2011, StatusNet, Inc
+/* ============================================================================
+ * Title: ActivityVerbHandlerPlugin
+ * Extends activity verb handling for plugin interfacing
  *
+ * postActiv:
+ * the micro-blogging software
+ *
+ * Copyright:
+ * Copyright (C) 2016, Maiyannah Bishop
+ *
+ * Derived from code copyright various sources:
+ * o GNU Social (C) 2013-2016, Free Software Foundation, Inc
+ * o StatusNet (C) 2008-2012, StatusNet, Inc
+ * ----------------------------------------------------------------------------
+ * License:
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -18,9 +26,30 @@
  *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * <https://www.gnu.org/licenses/agpl.html>
+ * ----------------------------------------------------------------------------
+ * About:
+ * Extends activity verb handling for plugin interfacing
+  *
+ * PHP version:
+ * Tested with PHP 5.6
+ * ----------------------------------------------------------------------------
+ * File Authors:
+ * o Mikael Nordfeldth <mmn@hethane.se>
+ * o Maiyannah Bishop <maiyannah.bishop@postactiv.com>
+ *
+ * Web:
+ *  o postActiv  <http://www.postactiv.com>
+ *  o GNU social <https://www.gnu.org/s/social/>
+ * ============================================================================
  */
+ 
+// This file is formatted so that it provides useful documentation output in
+// NaturalDocs.  Please be considerate of this before changing formatting.
 
-if (!defined('GNUSOCIAL')) { exit(1); }
+if (!defined('POSTACTIV')) { exit(1); }
+
 
 /**
  * @package     Activity
@@ -83,3 +112,7 @@ abstract class ActivityVerbHandlerPlugin extends ActivityHandlerPlugin
 
     abstract protected function getActivityForm(ManagedAction $action, $verb, Notice $target, Profile $scoped);
 }
+
+// END OF FILE
+// ============================================================================
+?>
