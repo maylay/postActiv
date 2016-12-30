db48e93f32edbbf229b402ad929d25439d91ceae
Maiyannah Bishop <maiyannah.bishop@postactiv.com>
2016-12-30 03:22:04 -0500

    Documentation for Spam_score

--- classes/activity/Spam_score.php
+++ classes/activity/Spam_score.php
@@ -51,173 +51,204 @@
 if (!defined('POSTACTIV')) { exit(1); }
 
 
-/**
- * Score of a notice per the activity spam service
- *
- * @category Spam
- * @package  StatusNet
- * @author   Evan Prodromou <evan@status.net>
- * @license  http://www.fsf.org/licensing/licenses/agpl.html AGPLv3
- * @link     http://status.net/
- *
- * @see      DB_DataObject
- */
-
+// ----------------------------------------------------------------------------
+// Class: Spam_score
+// Score of a notice per the activity spam service
+//
+// Defines:
+// o MAX_SCALE - 10000
+//
+// Variables:
+// o $__table   - 'spam_score'
+// o $notice_id - int
+// o $score     - float
+// o $created   - datetime
 class Spam_score extends Managed_DataObject
 {
-    const MAX_SCALE = 10000;
-    public $__table = 'spam_score'; // table name
+   const MAX_SCALE = 10000;
 
-    public $notice_id;   // int
-    public $score;       // float
-    public $created;     // datetime
+   public $__table = 'spam_score'; // table name
+   public $notice_id;   // int
+   public $score;       // float
+   public $created;     // datetime
 
-    function saveNew($notice, $result) {
 
-        $score = new Spam_score();
-
-        $score->notice_id      = $notice->id;
-        $score->score          = $result->probability;
-        $score->is_spam        = $result->isSpam;
-        $score->scaled         = Spam_score::scale($score->score);
-        $score->created        = common_sql_now();
-        $score->notice_created = $notice->created;
-
-        $score->insert();
-
-        self::blow('spam_score:notice_ids');
-
-        return $score;
-    }
-
-    function save($notice, $result) {
-
-        $orig  = null;
-        $score = Spam_score::getKV('notice_id', $notice->id);
-
-        if (empty($score)) {
-            $score = new Spam_score();
-        } else {
-            $orig = clone($score);
-        }
-
-        $score->notice_id      = $notice->id;
-        $score->score          = $result->probability;
-        $score->is_spam        = $result->isSpam;
-        $score->scaled         = Spam_score::scale($score->score);
-        $score->created        = common_sql_now();
-        $score->notice_created = $notice->created;
-
-        if (empty($orig)) {
-            $score->insert();
-        } else {
-            $score->update($orig);
-        }
-        
-        self::blow('spam_score:notice_ids');
-
-        return $score;
-    }
-
-    function delete($useWhere=false)
-    {
-        self::blow('spam_score:notice_ids');
-        self::blow('spam_score:notice_ids;last');
-        return parent::delete($useWhere);
-    }
-
-    /**
-     * The One True Thingy that must be defined and declared.
-     */
-    public static function schemaDef()
-    {
-        return array(
-            'description' => 'score of the notice per activityspam',
-            'fields' => array(
-                'notice_id' => array('type' => 'int',
-                                     'not null' => true,
-                                     'description' => 'notice getting scored'),
-                'score' => array('type' => 'double',
+   // Function: schemaDef
+   // Returns an array containing the layout of the database table for the
+   // purposes of maintenance tools to understand the database.
+   //
+   // Returns:
+   // o array
+   public static function schemaDef() {
+      return array(
+         'description' => 'score of the notice per activityspam',
+         'fields' => array(
+            'notice_id' => array('type' => 'int',
                                  'not null' => true,
-                                 'description' => 'score for the notice (0.0, 1.0)'),
-                'scaled' => array('type' => 'int',
-                                  'description' => 'scaled score for the notice (0, 10000)'),
-                'is_spam' => array('type' => 'tinyint',
-                                   'description' => 'flag for spamosity'),
-                'created' => array('type' => 'datetime',
-                                   'not null' => true,
-                                   'description' => 'date this record was created'),
-                'notice_created' => array('type' => 'datetime',
-                                          'description' => 'date the notice was created'),
-            ),
-            'primary key' => array('notice_id'),
+                                 'description' => 'notice getting scored'),
+            'score' => array('type' => 'double',
+                             'not null' => true,
+                             'description' => 'score for the notice (0.0, 1.0)'),
+            'scaled' => array('type' => 'int',
+                              'description' => 'scaled score for the notice (0, 10000)'),
+            'is_spam' => array('type' => 'tinyint',
+                               'description' => 'flag for spamosity'),
+            'created' => array('type' => 'datetime',
+                               'not null' => true,
+                               'description' => 'date this record was created'),
+            'notice_created' => array('type' => 'datetime',
+                                      'description' => 'date the notice was created')),
+         'primary key' => array('notice_id'),
             'foreign keys' => array(
-                'spam_score_notice_id_fkey' => array('notice', array('notice_id' => 'id')),
-            ),
-            'indexes' => array(
+                'spam_score_notice_id_fkey' => array('notice', array('notice_id' => 'id'))),
+         'indexes' => array(
                 'spam_score_created_idx' => array('created'),
-                'spam_score_scaled_idx' => array('scaled'),
-            ),
-        );
-    }
+                'spam_score_scaled_idx' => array('scaled')));
+   }
 
-    public static function upgrade()
-    {
-        Spam_score::upgradeScaled();
-        Spam_score::upgradeIsSpam();
-        Spam_score::upgradeNoticeCreated();
-    }
+   // -------------------------------------------------------------------------
+   // Function: saveNew
+   // Create a new spam score for a notice using a provider.  Called from the
+   // provider to do so.
+   //
+   // Parameters:
+   // o notice - notice the spam score is for
+   // o result - spam score of the notice
+   //
+   // Return:
+   // o Spam_score score - new Spam_score object
+   function saveNew($notice, $result) {
+      $score = new Spam_score();
+      $score->notice_id      = $notice->id;
+      $score->score          = $result->probability;
+      $score->is_spam        = $result->isSpam;
+      $score->scaled         = Spam_score::scale($score->score);
+      $score->created        = common_sql_now();
+      $score->notice_created = $notice->created;
+      $score->insert();
+      self::blow('spam_score:notice_ids');
+      return $score;
+   }
 
-    protected static function upgradeScaled()
-    {
-        $score = new Spam_score();
-        $score->whereAdd('scaled IS NULL');
-        
-        if ($score->find()) {
-            while ($score->fetch()) {
-                $orig = clone($score);
-                $score->scaled = Spam_score::scale($score->score);
-                $score->update($orig);
+
+   // -------------------------------------------------------------------------
+   // Function: save
+   // As saveNew but does duplication checking and a few other things.  If an
+   // existing spam score is there for the notice, we update it, rather than
+   // creating a new object.
+   //
+   // Parameters:
+   // o notice - notice the spam score is for
+   // o result - spam score of the notice
+   //
+   // Return:
+   // o Spam_score score - new Spam_score object
+   function save($notice, $result) {
+      $orig  = null;
+      $score = Spam_score::getKV('notice_id', $notice->id);
+      if (empty($score)) {
+         $score = new Spam_score();
+      } else {
+         $orig = clone($score);
+      }
+      $score->notice_id      = $notice->id;
+      $score->score          = $result->probability;
+      $score->is_spam        = $result->isSpam;
+      $score->scaled         = Spam_score::scale($score->score);
+      $score->created        = common_sql_now();
+      $score->notice_created = $notice->created;
+      if (empty($orig)) {
+            $score->insert();
+      } else {
+            $score->update($orig);
+      }
+      self::blow('spam_score:notice_ids');
+      return $score;
+   }
+
+
+   // -------------------------------------------------------------------------
+   // Function: delete
+   // Delete a spam score entry
+   function delete($useWhere=false) {
+      self::blow('spam_score:notice_ids');
+      self::blow('spam_score:notice_ids;last');
+      return parent::delete($useWhere);
+   }
+
+
+   // -------------------------------------------------------------------------
+   // Function: upgrade
+   // Perform some basic maintanence functions when ./scripts/upgrade.php is
+   // run.
+   public static function upgrade() {
+      Spam_score::upgradeScaled();
+      Spam_score::upgradeIsSpam();
+      Spam_score::upgradeNoticeCreated();
+   }
+
+
+   // -------------------------------------------------------------------------
+   // Function: upgradeScaled
+   // Finds entries in the database where the `scaled` attribute is NULL and
+   // updates them with the appropriate value.
+   protected static function upgradeScaled() {
+      $score = new Spam_score();
+      $score->whereAdd('scaled IS NULL');
+      if ($score->find()) {
+         while ($score->fetch()) {
+            $orig = clone($score);
+            $score->scaled = Spam_score::scale($score->score);
+            $score->update($orig);
+         }
+      }
+   }
+
+
+   // -------------------------------------------------------------------------
+   // Function: upgradeIsSpam
+   // Finds entries in the database where the `is_spam` attribute is NULL and
+   // updates them with the appropriate value.
+   protected static function upgradeIsSpam() {
+      $score = new Spam_score();
+      $score->whereAdd('is_spam IS NULL');
+      if ($score->find()) {
+         while ($score->fetch()) {
+            $orig = clone($score);
+            $score->is_spam = ($score->score >= 0.90) ? 1 : 0;
+            $score->update($orig);
+         }
+      }
+   }
+
+
+   // -------------------------------------------------------------------------
+   // Function: upgradeNoticeCreated
+   // Finds entries in the database where the `notice_created` attribute is
+   // NULL and updates them with the appropriate value.
+   protected static function upgradeNoticeCreated() {
+      $score = new Spam_score();
+      $score->whereAdd('notice_created IS NULL');
+      if ($score->find()) {
+         while ($score->fetch()) {
+            $notice = Notice::getKV('id', $score->notice_id);
+            if (!empty($notice)) {
+               $orig = clone($score);
+               $score->notice_created = $notice->created;
+               $score->update($orig);
             }
-        }
-    }
+         }
+      }
+   }
 
-    protected static function upgradeIsSpam()
-    {
-        $score = new Spam_score();
-        $score->whereAdd('is_spam IS NULL');
-        
-        if ($score->find()) {
-            while ($score->fetch()) {
-                $orig = clone($score);
-                $score->is_spam = ($score->score >= 0.90) ? 1 : 0;
-                $score->update($orig);
-            }
-        }
-    }
 
-    protected static function upgradeNoticeCreated()
-    {
-        $score = new Spam_score();
-        $score->whereAdd('notice_created IS NULL');
-        
-        if ($score->find()) {
-            while ($score->fetch()) {
-                $notice = Notice::getKV('id', $score->notice_id);
-                if (!empty($notice)) {
-                    $orig = clone($score);
-                    $score->notice_created = $notice->created;
-                    $score->update($orig);
-                }
-            }
-        }
-    }
-
-    public static function scale($score)
-    {
-        $raw = round($score * Spam_score::MAX_SCALE);
-        return max(0, min(Spam_score::MAX_SCALE, $raw));
-    }
+   // -------------------------------------------------------------------------
+   // Function: scale
+   // Scale the score given from a provider to our internal maximum.
+   public static function scale($score) {
+      $raw = round($score * Spam_score::MAX_SCALE);
+      return max(0, min(Spam_score::MAX_SCALE, $raw));
+   }
 }
 
 // END OF FILE
