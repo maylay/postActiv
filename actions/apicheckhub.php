aa54524c3dede0969a27b4bba95b702f81b12b6c
Maiyannah Bishop <maiyannah.bishop@postactiv.com>
2017-01-02 08:59:30 -0500

    NaturalDocs header for /actions/apicheckhub.php

--- actions/apicheckhub.php
+++ actions/apicheckhub.php
@@ -1,11 +1,19 @@
 <?php
 /* ============================================================================
- * postActiv - a fork of the GNU Social microblogging software
- * Copyright (C) 2016, Maiyannah Bishop
+ * Title: APICheckHub
+ * Check if a url has a push-hub, i.e. if it is possible to subscribe
+ *
+ * postActiv:
+ * the micro-blogging software
+ *
+ * Copyright:
+ * Copyright (C) 2016-2017, Maiyannah Bishop
+ *
  * Derived from code copyright various sources:
- *   GNU Social (C) 2013-2016, Free Software Foundation, Inc
- *   StatusNet (C) 2008-2012, StatusNet, Inc
+ * o GNU Social (C) 2013-2016, Free Software Foundation, Inc
+ * o StatusNet (C) 2008-2012, StatusNet, Inc
  * ----------------------------------------------------------------------------
+ * License:
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -18,28 +26,34 @@
  *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * <https://www.gnu.org/licenses/agpl.html>
  * ----------------------------------------------------------------------------
- * PHP version 5
+ * About:
+ * Check if a url has a push-hub, i.e. if it is possible to subscribe
  *
- * Show a notice's attachment
+ * PHP version:
+ * Tested with PHP 5.6, PHP 7
+ * ----------------------------------------------------------------------------
+ * File Authors:
+ * o Hannes Mannerheim <h@nnesmannerhe.im>
+ * o Mikael Nordfeldth <mmn@hethane.se>
+ * o Maiyannah Bishop <maiyannah.bishop@postactiv.com>
  *
- * @category  API
- * @package   postActiv
- * @author    Hannes Mannerheim <h@nnesmannerhe.im>
- * @author    Mikael Nordfeldth <mmn@hethane.se>
- * @author    Maiyannah Bishop <maiyannah.bishop@postactiv.com>
- * @copyright 2013-2016 Hannes Mannerheim
- * @copyright 2013-2016 Free Software Foundation, Inc.
- * @copyright 2016 Maiyannah Bishop
- * @license   https://www.gnu.org/licenses/agpl.html
- * @link      http://www.postactiv.com
- * ============================================================================ 
+ * Web:
+ *  o postActiv  <http://www.postactiv.com>
+ *  o GNU social <https://www.gnu.org/s/social/>
+ * ============================================================================
  */
+
+// This file is formatted so that it provides useful documentation output in
+// NaturalDocs.  Please be considerate of this before changing formatting.
 
 if (!defined('POSTACTIV')) { exit(1); }
 
+
 /**
- * Check if a url have a push-hub, i.e. if it is possible to subscribe
+ * Check if a url has a push-hub, i.e. if it is possible to subscribe
  *
  */
 class ApiCheckHubAction extends ApiAuthAction
@@ -123,4 +137,7 @@ class ApiCheckHubAction extends ApiAuthAction
         return true;
     }
 }
+
+// END OF FILE
+// ============================================================================
 ?>
