7b2acb3e2622590ac1495273bfdd91d13a98750e
Maiyannah Bishop <maiyannah.bishop@postactiv.com>
2017-01-02 07:02:28 -0500

    NaturalDocs header for /actions/apigroupismember.php

--- actions/apigroupismember.php
+++ actions/apigroupismember.php
@@ -1,11 +1,19 @@
 <?php
 /* ============================================================================
- * postActiv - a fork of the GNU Social microblogging software
- * Copyright (C) 2016, Maiyannah Bishop
+ * Title: APIGroupIsMember
+ * Returns whether a user is a member of a specified group.
+ *
+ * postActiv:
+ * the micro-blogging software
+ *
+ * Copyright:
+ * Copyright (C) 2016-2017, Maiyannah Bishop
+ *
  * Derived from code copyright various sources:
- *   GNU Social (C) 2013-2016, Free Software Foundation, Inc
- *   StatusNet (C) 2008-2012, StatusNet, Inc
+ * o GNU Social (C) 2013-2016, Free Software Foundation, Inc
+ * o StatusNet (C) 2008-2012, StatusNet, Inc
  * ----------------------------------------------------------------------------
+ * License:
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -18,26 +26,34 @@
  *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * ----------------------------------------------------------------------------
- * PHP version 5
  *
- * @category  API
- * @package   postActiv
- * @author    Zach Copley
- * @author    Siebrand Mazeland <s.mazeland@xs4all.nl>
- * @author    Eric Helgeson <erichelgeson@gmail.com>
- * @author    Craig Andrews <candrews@integralblue.com>
- * @author    Mikael Nordfeldth <mmn@hethane.se>
- * @author    Maiyannah Bishop <maiyannah.bishop@postactiv.com>
- * @copyright 2008-2012 StatusNet, Inc.
- * @copyright 2013-2016 Free Software Foundation, Inc
- * @copyright 2016 Maiyannah Bishop
- * @license   https://www.gnu.org/licenses/agpl.html
- * @link      http://www.postactiv.com
+ * <https://www.gnu.org/licenses/agpl.html>
+ * ----------------------------------------------------------------------------
+ * About:
+ * Returns whether a user is a member of a specified group.
+ *
+ * PHP version:
+ * Tested with PHP 5.6, PHP 7
+ * ----------------------------------------------------------------------------
+ * File Authors:
+ * o Zach Copley
+ * o Siebrand Mazeland <s.mazeland@xs4all.nl>
+ * o Eric Helgeson <erichelgeson@gmail.com>
+ * o Craig Andrews <candrews@integralblue.com>
+ * o Mikael Nordfeldth <mmn@hethane.se>
+ * o Maiyannah Bishop <maiyannah.bishop@postactiv.com>
+ *
+ * Web:
+ *  o postActiv  <http://www.postactiv.com>
+ *  o GNU social <https://www.gnu.org/s/social/>
  * ============================================================================
  */
+ 
+// This file is formatted so that it provides useful documentation output in
+// NaturalDocs.  Please be considerate of this before changing formatting.
 
 if (!defined('POSTACTIV')) { exit(1); }
+
 
 /**
  * Returns whether a user is a member of a specified group.
@@ -118,4 +134,7 @@ class ApiGroupIsMemberAction extends ApiBareAuthAction
         return true;
     }
 }
+
+// END OF FILE
+// ============================================================================
 ?>
