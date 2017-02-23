a9f1a27de8d0acd5c1462846b3cc16d6b5d5c3ff
Maiyannah Bishop <maiyannah.bishop@postactiv.com>
2017-02-23 08:36:10 -0500

    1.9.2 is latest DB release

--- extlib/DB/mysqli.php
+++ extlib/DB/mysqli.php
@@ -43,7 +43,7 @@ require_once 'DB/common.php';
  * @author     Daniel Convissor <danielc@php.net>
  * @copyright  1997-2007 The PHP Group
  * @license    http://www.php.net/license/3_0.txt  PHP License 3.0
- * @version    Release: 1.9.1
+ * @version    Release: 1.9.2
  * @link       http://pear.php.net/package/DB
  * @since      Class functional since Release 1.6.3
  */
@@ -497,7 +497,11 @@ class DB_mysqli extends DB_common
      */
     function freeResult($result)
     {
-        return is_resource($result) ? mysqli_free_result($result) : false;
+        if (! $result instanceof mysqli_result) {
+            return false;
+        }
+        mysqli_free_result($result);
+        return true;
     }
 
     // }}}
