bca2ce28fff39d4d523f3720be1bd5a2f1a7429d
Maiyannah Bishop <maiyannah.bishop@postactiv.com>
2017-03-14 05:23:37 -0400

    Full documentation for actions/sup.php

--- actions/sup.php
+++ actions/sup.php
@@ -53,74 +53,103 @@
 
 if (!defined('POSTACTIV')) { exit(1); }
 
-// @todo FIXME: documentation needed.
-class SupAction extends Action
-{
-    function handle()
-    {
-        parent::handle();
 
-        $seconds = $this->trimmed('seconds');
+// ============================================================================
+// Class: SupAction
+// Action class to update a stream based on a given interval
+class SupAction extends Action {
+   // -------------------------------------------------------------------------
+   // Function: handle
+   // Get updates for the stream based on the update period specified.
+   // Prints a json encoded string with them.
+   //
+   // Parameters:
+   // o none
+   //
+   // Returns:
+   // o void
+   function handle()
+   {
+      parent::handle();
+      $seconds = $this->trimmed('seconds');
+      if (!$seconds) {
+         $seconds = 15;
+      }
+      $updates = $this->getUpdates($seconds);
+      header('Content-Type: application/json; charset=utf-8');
+      print json_encode(array('updated_time' => date('c'),
+                              'since_time' => date('c', time() - $seconds),
+                              'available_periods' => $this->availablePeriods(),
+                              'period' => $seconds,
+                              'updates' => $updates));
+   }
 
-        if (!$seconds) {
-            $seconds = 15;
-        }
 
-        $updates = $this->getUpdates($seconds);
+   // -------------------------------------------------------------------------
+   // Function: availablePeriods
+   // Determines which of the various HARDCODED arbitrary periods our interval
+   // is closest to and returns that.
+   //
+   // Parameters:
+   // o none
+   //
+   // Returns:
+   // o $array - periods we can use based on the given desired interval
+   function availablePeriods() {
+      static $periods = array(86400, 43200, 21600, 7200,
+                              3600, 1800, 600, 300, 120,
+                              60, 30, 15);
+      $available = array();
+      foreach ($periods as $period) {
+         $available[$period] = common_local_url('sup',
+                                                array('seconds' => $period));
+      }
+      return $available;
+   }
 
-        header('Content-Type: application/json; charset=utf-8');
 
-        print json_encode(array('updated_time' => date('c'),
-                                'since_time' => date('c', time() - $seconds),
-                                'available_periods' => $this->availablePeriods(),
-                                'period' => $seconds,
-                                'updates' => $updates));
-    }
+   // --------------------------------------------------------------------------
+   // Function: getUpdates
+   // Fetches and returns new notices for a stream since the last update
+   //
+   // Parameters:
+   // o seconds - update interval
+   //
+   // Returns:
+   // o array %updates - new array of ids of the new notices
+   function getUpdates($seconds) {
+      $notice = new Notice();
+      $divider = common_sql_date(time() - $seconds);
+      $notice->query('SELECT profile_id, max(id) AS max_id ' .
+                     'FROM ( ' .
+                     'SELECT profile_id, id FROM notice ' .
+                      ((common_config('db','type') == 'pgsql') ?
+                     'WHERE extract(epoch from created) > (extract(epoch from now()) - ' . $seconds . ') ' :
+                     'WHERE created > "'.$divider.'" ' ) .
+                     ') AS latest ' .
+                     'GROUP BY profile_id');
+      $updates = array();
+      while ($notice->fetch()) {
+         $updates[] = array($notice->profile_id, $notice->max_id);
+      }
+      return $updates;
+   }
 
-    function availablePeriods()
-    {
-        static $periods = array(86400, 43200, 21600, 7200,
-                                3600, 1800, 600, 300, 120,
-                                60, 30, 15);
-        $available = array();
-        foreach ($periods as $period) {
-            $available[$period] = common_local_url('sup',
-                                                   array('seconds' => $period));
-        }
 
-        return $available;
-    }
-
-    function getUpdates($seconds)
-    {
-        $notice = new Notice();
-
-        // XXX: cache this. Depends on how big this protocol becomes;
-        // Re-doing this query every 15 seconds isn't the end of the world.
-
-        $divider = common_sql_date(time() - $seconds);
-
-        $notice->query('SELECT profile_id, max(id) AS max_id ' .
-                       'FROM ( ' .
-                       'SELECT profile_id, id FROM notice ' .
-                        ((common_config('db','type') == 'pgsql') ?
-                       'WHERE extract(epoch from created) > (extract(epoch from now()) - ' . $seconds . ') ' :
-                       'WHERE created > "'.$divider.'" ' ) .
-                       ') AS latest ' .
-                       'GROUP BY profile_id');
-
-        $updates = array();
-
-        while ($notice->fetch()) {
-            $updates[] = array($notice->profile_id, $notice->max_id);
-        }
-
-        return $updates;
-    }
-
-    function isReadOnly($args)
-    {
-        return true;
-    }
+   // -------------------------------------------------------------------------
+   // Function: isReadOnly
+   // Returns whether the action class is read only (yes)
+   //
+   // Parameters:
+   // o array $args - ignored
+   //
+   // Returns:
+   // o boolean true
+   function isReadOnly($args) {
+      return true;
+   }
 }
+
+// END OF FILE
+// ============================================================================
 ?>
