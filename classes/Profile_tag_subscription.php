16b7b8706175bc7948a427474d42501e332376b1
David Yip <yipdw@member.fsf.org>
2017-04-08 02:02:59 -0500

    Remove stray comment markers in various classes
    These cause syntax errors when provisioning the apache-php7-mariadb
    Vagrant image; probably elsewhere also.

--- classes/Profile_tag_subscription.php
+++ classes/Profile_tag_subscription.php
@@ -50,7 +50,7 @@
  
 // This file is formatted so that it provides useful documentation output in
 // NaturalDocs.  Please be considerate of this before changing formatting.
- */
+
 require_once INSTALLDIR.'/classes/Memcached_DataObject.php';
 
 class Profile_tag_subscription extends Managed_DataObject
