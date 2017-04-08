1c93c2f590a71092e04c8ed4163f8b58b7bd12c0
Normandy <kuroe@openmailbox.org>
2017-04-08 00:41:23 -0400

    Added documentation for OpportunisticQM

--- modules/OpportunisticQM/actions/runqueue.php
+++ modules/OpportunisticQM/actions/runqueue.php
@@ -1,11 +1,19 @@
 <?php
-/*
- * postActiv - a fork of the GNU Social microblogging software
- * Copyright (C) 2016, Maiyannah Bishop
- * Derived from code copyright various sources:
- *   GNU Social (C) 2013-2016, Free Software Foundation, Inc
- *   StatusNet (C) 2008-2011, StatusNet, Inc
+/* ============================================================================
+ * Title: RunQueue
+ * Definition for an action to run the queue
  *
+ * postActiv:
+ * the micro-blogging software
+ *
+ * Copyright:
+ * Copyright (C) 2016-2017, Maiyannah Bishop
+ *
+ * Derived from code copyright various sources:
+ * o GNU Social (C) 2013-2016, Free Software Foundation, Inc
+ * o StatusNet (C) 2008-2012, StatusNet, Inc
+ * ----------------------------------------------------------------------------
+ * License:
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -19,15 +27,47 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- * @license   https://www.gnu.org/licenses/agpl.html 
+ * <https://www.gnu.org/licenses/agpl.html>
+ * ----------------------------------------------------------------------------
+ * About:
+ * Definition for an action to run the queue
+ *
+ * PHP version:
+ * Tested with PHP 7
+ * ----------------------------------------------------------------------------
+ * File Authors:
+ * o Mikael Nordfeldth <mmn@hethane.se>
+ * o Maiyannah Bishop <maiyannah.bishop@postactiv.com>
+ *
+ * Web:
+ *  o postActiv  <http://www.postactiv.com>
+ *  o GNU social <https://www.gnu.org/s/social/>
+ * ============================================================================
  */
+
+// This file is formatted so that it provides useful documentation output in
+// NaturalDocs.  Please be considerate of this before changing formatting.
 
 if (!defined('GNUSOCIAL')) { exit(1); }
 
+// -------------------------------------------------------------------------
+// Class: RunqueueAction
+//
+// Variables:
+// o $qm - QueueManager instance
 class RunqueueAction extends Action
 {
     protected $qm = null;
 
+    // --------------------------------------------------------------
+    // Function: prepare
+    // For initializing members of the class.
+    //
+    // Parameters:
+    // o array $args - Arguments array
+    //
+    // Returns:
+    // o boolean true
     protected function prepare(array $args=array())
     {
         parent::prepare($args);
@@ -51,6 +91,9 @@ class RunqueueAction extends Action
         return true;
     }
 
+    // --------------------------------------------------------------
+    // Function: handle
+    // Queue handler method
     protected function handle() {
         // We don't need any of the parent functionality from parent::handle() here.
 
