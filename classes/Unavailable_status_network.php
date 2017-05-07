dbba590d1c781e02d80c8a5e437190cf890dc070
Maiyannah Bishop <maiyannah.bishop@postactiv.com>
2017-05-07 09:28:26 -0400

    Cleanup Unavailable_status_network code documentation/formatting

--- classes/Unavailable_status_network.php
+++ classes/Unavailable_status_network.php
@@ -51,36 +51,29 @@
 
 if (!defined('POSTACTIV')) { exit(1); }
 
-/**
- * Keeps a list of unavailable status network names
- *
- * @see      Managed_DataObject
- */
 
-class Unavailable_status_network extends Managed_DataObject
-{
-    public $__table = 'unavailable_status_network'; // table name
+// ============================================================================
+// Class: Unavailable_status_network
+// Keeps a list of unavailable status network names
+class Unavailable_status_network extends Managed_DataObject {
+   public $__table = 'unavailable_status_network'; // table name
+   public $nickname;              // varchar(64) UUID
+   public $created;               // datetime
 
-    public $nickname;              // varchar(64) UUID
-    public $created;               // datetime
-
-    /**
-     * The One True Thingy that must be defined and declared.
-     */
-    public static function schemaDef()
-    {
-        return array(
-            'description' => 'An unavailable status network nickname',
-            'fields' => array(
-                'nickname' => array('type' => 'varchar',
-                                    'length' => 64,
-                                    'not null' => true, 'description' => 'nickname not to use'),
-                'created' => array('type' => 'datetime',
-                                   'not null' => true),
-            ),
-            'primary key' => array('nickname'),
-        );
-    }
+   // -------------------------------------------------------------------------
+   // Function: schemaDef
+   // The One True Thingy that must be defined and declared.
+   public static function schemaDef() {
+      return array(
+         'description' => 'An unavailable status network nickname',
+         'fields' => array(
+            'nickname' => array('type' => 'varchar',
+                                'length' => 64,
+                                'not null' => true, 'description' => 'nickname not to use'),
+            'created' => array('type' => 'datetime',
+                               'not null' => true),),
+         'primary key' => array('nickname'),);
+   }
 }
 
 // END OF FILE
