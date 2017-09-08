329655bd954ceb8667c879bccfc3ffcaecacb346
Maiyannah Bishop <maiyannah.bishop@postactiv.com>
2017-09-08 19:58:53 -0400

    Add verify_magic_envelope function, some docs, to FederateDiaspora

--- modules/FederateDiaspora/FederateDiaspora.php
+++ modules/FederateDiaspora/FederateDiaspora.php
@@ -53,13 +53,19 @@
 // This file is formatted so that it provides useful documentation output in
 // NaturalDocs.  Please be considerate of this before changing formatting.
 
+// ============================================================================
+// Class: Diaspora
+// Federation module to federate with the v2 Diaspora protocol.
+//
+// 100% experimental at this point and mostly shamelessly lifted from the
+// Friendica code.
 class Diaspora extends FederationModule {
 
    // -------------------------------------------------------------------------
    // Function: repair_signature
    // Repairs a signature that was double encoded
-	// The function is unused at the moment. It was copied from the old implementation.
-	//
+   // The function is unused at the moment. It was copied from the old implementation.
+   //
    // Parameters:
    // o string $signature The signature
    // o string $handle The handle of the signature owner
@@ -67,7 +73,7 @@ class Diaspora extends FederationModule {
    //
    // Returns:
    // o string the repaired signature
-	private static function repair_signature($signature, $handle = "", $level = 1) {
+   private static function repair_signature($signature, $handle = "", $level = 1) {
       if ($signature == "")
          return ($signature);
       if (base64_encode(base64_decode(base64_decode($signature))) == base64_decode($signature)) {
@@ -79,4 +85,49 @@ class Diaspora extends FederationModule {
       }
       return($signature);
    }
+
+   // -------------------------------------------------------------------------
+   // Function: verify_magic_envelope
+   // Verifies the envelope and return the verified data
+   //
+   // Parameters:
+   // o string $envelope The magic envelope
+   //
+   // Returns:
+   // o string verified data
+   private static function verify_magic_envelope($envelope) {
+      $basedom = parse_xml_string($envelope, false);
+      if (!is_object($basedom)) {
+         common_debug("Envelope is no XML file");
+         return false;
+      }
+      $children = $basedom->children('http://salmon-protocol.org/ns/magic-env');
+      if (sizeof($children) == 0) {
+         common_debug("XML has no children");
+         return false;
+      }
+      $handle    = "";
+      $data      = base64url_decode($children->data);
+      $type      = $children->data->attributes()->type[0];
+      $encoding  = $children->encoding;
+      $alg       = $children->alg;
+      $sig       = base64url_decode($children->sig);
+      $key_id    = $children->sig->attributes()->key_id[0];
+      if ($key_id != "")
+         $handle = base64url_decode($key_id);
+
+      $b64url_data = base64url_encode($data);
+
+      $msg           = str_replace(array("\n", "\r", " ", "\t"), array("", "", "", ""), $b64url_data);
+      $signable_data = $msg.".".base64url_encode($type).".".base64url_encode($encoding).".".base64url_encode($alg);
+      $key           = self::key($handle);
+      $verify        = rsa_verify($signable_data, $sig, $key);
+      if (!$verify) {
+         common_log('Diaspora federation: Message did not verify. Discarding.');
+         return false;
+      }
+      return $data;
+   }
 }
+
+?>
