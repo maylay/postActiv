c61c061a30e540377aa83db9a2fe4258d07b6f95
Maiyannah Bishop <maiyannah.bishop@postactiv.com>
2017-09-12 20:22:25 -0400

    Coding standards pass in ActivityModeration module

--- modules/ActivityModeration/forms/deletenotice.php
+++ modules/ActivityModeration/forms/deletenotice.php
@@ -1,11 +1,19 @@
 <?php
-/***
- * postActiv - a fork of the GNU Social microblogging software
- * Copyright (C) 2016, Maiyannah Bishop
- * Derived from code copyright various sources:
- *   GNU Social (C) 2013-2016, Free Software Foundation, Inc
- *   StatusNet (C) 2008-2012, StatusNet, Inc
+/* ============================================================================
+ * Title: DeleteNotice
+ * Form to delete a notice
  *
+ * postActiv:
+ * the micro-blogging software
+ *
+ * Copyright:
+ * Copyright (C) 2016-2017, Maiyannah Bishop
+ *
+ * Derived from code copyright various sources:
+ * o GNU Social (C) 2013-2016, Free Software Foundation, Inc
+ * o StatusNet (C) 2008-2012, StatusNet, Inc
+ * ----------------------------------------------------------------------------
+ * License:
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -18,70 +26,67 @@
  *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * PHP version 5
- *
+ * ----------------------------------------------------------------------------
+ * About:
  * Form to delete a notice
  *
- * @category  ActivityVerb
- * @package   postActiv
- * @author    Maiyannah Bishop <maiyannah.bishop@postactiv.com>
- * @copyright 2012 StatusNet, Inc.
- * @copyright 2013-2016 Free Software Foundation, Inc.
- * @copyright 2016 Maiyannah Bishop
- * @license   https://www.gnu.org/licenses/agpl.html
- * @link      http://www.postactiv.com/
+ * PHP version:
+ * Tested with PHP 7
+ * ----------------------------------------------------------------------------
+ * File Authors:
+ * o Mikael Nordfeldth <mmn@hethane.se>
+ * o Maiyannah Bishop <maiyannah.bishop@postactiv.com>
+ *
+ * Web:
+ *  o postActiv  <http://www.postactiv.com>
+ *  o GNU social <https://www.gnu.org/s/social/>
+ * ============================================================================
  */
+ 
+// This file is formatted so that it provides useful documentation output in
+// NaturalDocs.  Please be considerate of this before changing formatting.
 
-if (!defined('GNUSOCIAL')) { exit(1); }
+if (!defined('POSTACTIV')) { exit(1); }
 
-class DeletenoticeForm extends Form
-{
-    protected $notice = null;
+class DeletenoticeForm extends Form {
+   protected $notice = null;
 
-    function __construct(HTMLOutputter $out=null, array $formOpts=array())
-    {
-        if (!array_key_exists('notice', $formOpts) || !$formOpts['notice'] instanceof Notice) {
-            throw new ServerException('No notice provided to DeletenoticeForm');
-        }
+   function __construct(HTMLOutputter $out=null, array $formOpts=array()) {
+      if (!array_key_exists('notice', $formOpts) || !$formOpts['notice'] instanceof Notice) {
+         throw new ServerException('No notice provided to DeletenoticeForm');
+      }
 
-        parent::__construct($out);
+      parent::__construct($out);
+      $this->notice = $formOpts['notice'];
+   }
 
-        $this->notice = $formOpts['notice'];
-    }
+   function id() {
+      return 'form_notice_delete-' . $this->notice->getID();
+   }
 
-    function id()
-    {
-        return 'form_notice_delete-' . $this->notice->getID();
-    }
+   function formClass() {
+      return 'form_settings';
+   }
 
-    function formClass()
-    {
-        return 'form_settings';
-    }
+   function action() {
+      return common_local_url('deletenotice', array('notice' => $this->notice->getID()));
+   }
 
-    function action()
-    {
-        return common_local_url('deletenotice', array('notice' => $this->notice->getID()));
-    }
+   function formLegend() {
+      $this->out->element('legend', null, _('Delete notice'));
+   }
 
-    function formLegend()
-    {
-        $this->out->element('legend', null, _('Delete notice'));
-    }
+   function formData() {
+      $this->out->element('p', null, _('Are you sure you want to delete this notice?'));
+   }
 
-    function formData()
-    {
-        $this->out->element('p', null, _('Are you sure you want to delete this notice?'));
-    }
-
-    /**
-     * Action elements
-     *
-     * @return void
-     */
-    function formActions()
-    {
+   /**
+   * Action elements
+   *
+   * @return void
+   */
+   function formActions()
+   {
         $this->out->submit('form_action-no',
                       // TRANS: Button label on the delete notice form.
                       _m('BUTTON','No'),
@@ -96,5 +101,9 @@ class DeletenoticeForm extends Form
                       'yes',
                       // TRANS: Submit button title for 'Yes' when deleting a notice.
                       _('Delete this notice.'));
-    }
+   }
 }
+
+// END OF FILE
+// ============================================================================
+?>
