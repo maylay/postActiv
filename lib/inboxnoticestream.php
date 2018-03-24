77e571a794c9b9e11e8d457163d76a0cff5d1f67
Maiyannah Bishop <maiyannah.bishop@postactiv.com>
2018-03-24 10:58:20 -0400

    Code standards pass on this file since we're here patching.

--- lib/inboxnoticestream.php
+++ lib/inboxnoticestream.php
@@ -1,15 +1,19 @@
 <?php
-/***
- * postActiv - a fork of the GNU Social microblogging software
- * Copyright (C) 2016, Maiyannah Bishop
- * Derived from code copyright various sources:
- *   GNU Social (C) 2013-2016, Free Software Foundation, Inc
- *   StatusNet (C) 2008-2011, StatusNet, Inc
- *
+/* ============================================================================
+ * Title: Inbox Notice Stream
  * Stream of notices for a profile's "all" feed
  *
- * PHP version 5
+ * postActiv:
+ * the micro-blogging software
  *
+ * Copyright:
+ * Copyright (C) 2016-2018, Maiyannah Bishop
+ *
+ * Derived from code copyright various sources:
+ * o GNU Social (C) 2013-2016, Free Software Foundation, Inc
+ * o StatusNet (C) 2008-2012, StatusNet, Inc
+ * ----------------------------------------------------------------------------
+ * License:
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -22,143 +26,146 @@
  *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * ----------------------------------------------------------------------------
+ * About:
+ * A specific user's "all" feed.
  *
- * @category  NoticeStream
- * @package   StatusNet
- * @author    Evan Prodromou <evan@status.net>
- * @author    Mikael Nordfeldth <mmn@hethane.se>
- * @author    Alexi Sorokin <sor.alexei@meowr.ru>
- * @copyright 2011 StatusNet, Inc.
- * @copyright 2014 Free Software Foundation, Inc.
- * @license   https://www.gnu.org/licenses/agpl.html
- * @link      http://status.net/
+ * PHP version:
+ * Tested with PHP 7
+ * ----------------------------------------------------------------------------
+ * File Authors:
+ *  o Evan Prodromou <evan@status.net>
+ *  o Mikael Nordfeldth <mmn@hethane.se>
+ *  o Alexi Sorokin <sor.alexei@meowr.ru>
+ *  o Maiyannah Bishop <maiyannah.bishop@postactiv.com>
+ * ============================================================================
  */
+
+// This file is formatted so that it provides useful documentation output in
+// NaturalDocs.  Please be considerate of this before changing formatting.
 
 if (!defined('POSTACTIV')) { exit(1); }
 
-/**
- * Stream of notices for a profile's "all" feed
- */
-class InboxNoticeStream extends ScopingNoticeStream
-{
-    /**
-     * Constructor
-     *
-     * @param Profile $target Profile to get a stream for
-     * @param Profile $scoped Currently scoped profile (if null, it is fetched)
-     */
-    function __construct(Profile $target, Profile $scoped=null)
-    {
-        // FIXME: we don't use CachingNoticeStream - but maybe we should?
-        parent::__construct(new CachingNoticeStream(new RawInboxNoticeStream($target), 'profileall'), $scoped);
-    }
+// ============================================================================
+// Stream of notices for a profile's "all" feed
+class InboxNoticeStream extends ScopingNoticeStream {
+   // -------------------------------------------------------------------------
+   // Function: __construct
+   // Class object constructor
+   //
+   // Parameters:
+   // o Profile $target - Profile to get a stream for
+   // o Profile $scoped - Currently scoped profile (if null, it is fetched)
+   //
+   // FIXME:
+   // o we don't use CachingNoticeStream - but maybe we should?
+   function __construct(Profile $target, Profile $scoped=null) {
+      parent::__construct(new CachingNoticeStream(new RawInboxNoticeStream($target), 'profileall'), $scoped);
+   }
 }
 
-/**
- * Raw stream of notices for the target's inbox
- *
- * @category  General
- * @package   StatusNet
- * @author    Evan Prodromou <evan@status.net>
- * @copyright 2011 StatusNet, Inc.
- * @license   http://www.fsf.org/licensing/licenses/agpl-3.0.html AGPL 3.0
- * @link      http://status.net/
- */
-class RawInboxNoticeStream extends FullNoticeStream
-{
-    protected $target  = null;
-    protected $inbox = null;
+// ============================================================================
+// Class: RawInboxNoticeStream
+// Raw stream of notices for the target's inbox
+class RawInboxNoticeStream extends FullNoticeStream {
+   protected $target = null;
+   protected $inbox  = null;
 
-    /**
-     * Constructor
-     *
-     * @param Profile $target Profile to get a stream for
-     */
-    function __construct(Profile $target)
-    {
-        parent::__construct();
-        $this->target  = $target;
-    }
+   // -------------------------------------------------------------------------
+   // Function: __construct
+   // Class object constructor
+   //
+   // Parameters:
+   // o Profile $target Profile to get a stream for
+   function __construct(Profile $target) {
+      parent::__construct();
+      $this->target  = $target;
+   }
 
-    /**
-     * Get IDs in a range
-     *
-     * @param int $offset   Offset from start
-     * @param int $limit    Limit of number to get
-     * @param int $since_id Since this notice
-     * @param int $max_id   Before this notice
-     *
-     * @return Array IDs found
-     */
-    function getNoticeIds($offset, $limit, $since_id, $max_id)
-    {
-        $notice_ids = array();
+   // -------------------------------------------------------------------------
+   // Function: getNoticeIds
+   // Get IDs in a range
+   //
+   // Parameters:
+   // o int $offset   - Offset from start
+   // o int $limit    - Limit of number to get
+   // o int $since_id - Since this notice
+   // o int $max_id   - Before this notice
+   //
+   // Returns:
+   // o Array IDs found
+   function getNoticeIds($offset, $limit, $since_id, $max_id) {
+      $notice_ids = array();
 
-        // Subscription:: is a table of subscriptions (every user is subscribed to themselves)
-        $subscription = new Subscription();
-        $subscription->selectAdd();
-        $subscription->selectAdd('subscribed');
-        $subscription->whereAdd(sprintf('subscriber = %1$d', $this->target->id));
-        $subscription_profile_ids = $subscription->fetchAll('subscribed');
+      // Subscription:: is a table of subscriptions (every user is subscribed to themselves)
+      $subscription = new Subscription();
+      $subscription->selectAdd();
+      $subscription->selectAdd('subscribed');
+      $subscription->whereAdd(sprintf('subscriber = %1$d', $this->target->id));
+      $subscription_profile_ids = $subscription->fetchAll('subscribed');
 
-        // Reply:: is a table of mentions
-        $reply = new Reply();
-        $reply->selectAdd();
-        $reply->selectAdd('notice_id');
-        $reply->whereAdd(sprintf('profile_id = %1$d', $this->target->id));
-        $notice_ids += $reply->fetchAll('notice_id');
+      // Reply:: is a table of mentions
+      $reply = new Reply();
+      $reply->selectAdd();
+      $reply->selectAdd('notice_id');
+      $reply->whereAdd(sprintf('profile_id = %1$d', $this->target->id));
+      $notice_ids += $reply->fetchAll('notice_id');
 
-        $attention = new Attention();
-        $attention->selectAdd();
-        $attention->selectAdd('notice_id');
-        $attention->whereAdd(sprintf('profile_id = %1$d', $this->target->id));
-        $notice_ids += $attention->fetchAll('notice_id');
+      $attention = new Attention();
+      $attention->selectAdd();
+      $attention->selectAdd('notice_id');
+      $attention->whereAdd(sprintf('profile_id = %1$d', $this->target->id));
+      $notice_ids += $attention->fetchAll('notice_id');
 
-        $group_inbox = new Group_inbox();
-        $group_inbox->selectAdd();
-        $group_inbox->selectAdd('notice_id');
-        $group_inbox->whereAdd(
-                sprintf('group_id IN (SELECT group_id FROM group_member WHERE profile_id = %1$d)',
-                        $this->target->id));
-        $notice_ids += $group_inbox->fetchAll('notice_id');
+      $group_inbox = new Group_inbox();
+      $group_inbox->selectAdd();
+      $group_inbox->selectAdd('notice_id');
+      $group_inbox->whereAdd(
+         sprintf('group_id IN (SELECT group_id FROM group_member WHERE profile_id = %1$d)',
+            $this->target->id));
+      $notice_ids += $group_inbox->fetchAll('notice_id');
 
-        $query_ids = '';
-        if (!empty($notice_ids)) {
-            $query_ids .= 'notice.id IN (' . implode(', ', $notice_ids) . ') OR ';
-        }
-        $query_ids .= 'notice.profile_id IN (' . implode(', ', $subscription_profile_ids) . ')';
+      $query_ids = '';
+      
+      if (!empty($notice_ids)) {
+         $query_ids .= 'notice.id IN (' . implode(', ', $notice_ids) . ') OR ';
+      }
+      $query_ids .= 'notice.profile_id IN (' . implode(', ', $subscription_profile_ids) . ')';
 
-        $notice = new Notice();
-        $notice->selectAdd();
-        $notice->selectAdd('id');
-        $notice->whereAdd(sprintf('notice.created > "%s"', $notice->escape($this->target->created)));
-        if (!is_null($query_ids)) {
-            $notice->whereAdd($query_ids);
-        }
-        if (!empty($since_id)) {
-            $notice->whereAdd(sprintf('notice.id > %d', $since_id));
-        }
-        if (!empty($max_id)) {
-            $notice->whereAdd(sprintf('notice.id <= %d', $max_id));
-        }
+      $notice = new Notice();
+      $notice->selectAdd();
+      $notice->selectAdd('id');
+      $notice->whereAdd(sprintf('notice.created > "%s"', $notice->escape($this->target->created)));
+      
+      if (!is_null($query_ids)) {
+         $notice->whereAdd($query_ids);
+      }
+      if (!empty($since_id)) {
+         $notice->whereAdd(sprintf('notice.id > %d', $since_id));
+      }
+      if (!empty($max_id)) {
+         $notice->whereAdd(sprintf('notice.id <= %d', $max_id));
+      }
 
-        self::filterVerbs($notice, $this->selectVerbs);
+      self::filterVerbs($notice, $this->selectVerbs);
 
-        $notice->limit($offset, $limit);
-        // notice.id will give us even really old posts, which were
-        // recently imported. For example if a remote instance had
-        // problems and just managed to post here. Another solution
-        // would be to have a 'notice.imported' field and order by it.
-        $notice->orderBy('notice.id DESC');
+      $notice->limit($offset, $limit);
+      // notice.id will give us even really old posts, which were
+      // recently imported. For example if a remote instance had
+      // problems and just managed to post here. Another solution
+      // would be to have a 'notice.imported' field and order by it.
+      $notice->orderBy('notice.id DESC');
 
-        if (!$notice->find()) {
-            return array();
-        }
+      if (!$notice->find()) {
+         return array();
+      }
 
-        $ids = $notice->fetchAll('id');
+      $ids = $notice->fetchAll('id');
 
-        return $ids;
-    }
+      return $ids;
+   }
 }
 
+// END OF FILE
+// ============================================================================
 ?>
